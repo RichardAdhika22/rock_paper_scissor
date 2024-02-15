f2177706314c49756baab70aa45151ff5f5266bb
Richard Adhika <adhikarichard@gmail.com>
2024-02-15 11:19:05 -0800

    Added css

--- script.js
+++ script.js
@@ -8,9 +8,42 @@ function getcomputerSelection(){
         case 2:
             return "scissors";
     }
+    if (computerValue >= 3) {
+        return "scissors";
+    }
+}
+
+function displayChoice(selection, player) {
+    const eraseP = document.querySelector('.displayP');
+    const eraseC = document.querySelector('.displayC');
+    if(eraseP != null && player) playerChoice.removeChild(eraseP);
+    if(eraseC != null && !player) computerChoice.removeChild(eraseC);
+
+    var displayImg;
+    var displayImg = document.createElement('img');
+    if (selection == "paper") {
+        displayImg.src = "images/paper.avif";
+    } else if (selection == "scissors") {
+        displayImg.src = "images/scissors.avif";
+    } else {
+        displayImg.src = "images/rock.png";
+    }
+    
+    displayImg.style.height = "100px";
+    displayImg.style.opacity = "80%";
+
+    if (player) {
+        displayImg.classList.add('displayP');
+        playerChoice.appendChild(displayImg);
+    }
+    else {
+        displayImg.classList.add('displayC');
+        computerChoice.appendChild(displayImg);
+    }
 }
 
 function playRound(playerSelection, computerSelection){
+    displayChoice(playerSelection, true);
     if (playerSelection == "paper"){
         if(computerSelection == "paper"){
             return("It's a tie! Both choose Paper");
@@ -48,15 +81,27 @@ function playRound(playerSelection, computerSelection){
 
 function game(playerSelection){
     let computerSelection = getcomputerSelection();
+    displayChoice(computerSelection, false);
     return playRound(playerSelection, computerSelection);
 }
 
 let playerScore = 0, computerScore = 0;
 let finished, result='';
 
-const choices = document.querySelectorAll('button');
+const battleResult = document.getElementById('result');
+const resultMsg = document.getElementById('resultMsg');
+const playerResult = document.getElementById('player-result');
+const playerChoice = document.getElementById('player-choice');
+const computerChoice = document.getElementById('computer-choice');
+const computerResult = document.getElementById('computer-result');
+const final = document.getElementById('final');
+
+const choices = document.querySelectorAll('input');
 choices.forEach((choice) => {
     choice.addEventListener('click', () => {
+        // const erase = document.querySelector('.display');
+        // if (erase != null) battleResult.removeChild(erase);
+
         let roundResult = game(choice.id);
 
         playerScore += (roundResult.split("Win").length -1);
@@ -71,28 +116,31 @@ choices.forEach((choice) => {
             finished = true;
         }
 
-        const body = document.querySelector('body');
-        const erase = document.querySelector('.score');
+        // const body = document.querySelector('body');
+        // const erase = document.querySelector('.score');
 
-        if(erase != null) body.removeChild(erase);
+        // if(erase != null) body.removeChild(erase);
  
-        const displayScore = document.createElement('div');
-        displayScore.classList.add('score');
-        const displayPlayer = document.createElement('div');
-        displayPlayer.textContent = `Your Score = ${playerScore}`;
-        const displayComputer = document.createElement('div');
-        displayComputer.textContent = `Computer's Score = ${computerScore}`;
+        // const displayScore = document.createElement('div');
+        // displayScore.classList.add('score');
+        // const displayPlayer = document.createElement('div');
+        // displayPlayer.textContent = `Your Score = ${playerScore}`;
+        // const displayComputer = document.createElement('div');
+        // displayComputer.textContent = `Computer's Score = ${computerScore}`;
 
-        body.appendChild(displayScore);
-        displayScore.appendChild(displayPlayer);
-        displayScore.appendChild(displayComputer);
+        resultMsg.textContent = roundResult;
+        playerResult.textContent = `Player: ${playerScore}`;
+        computerResult.textContent = `Computer: ${computerScore}`;
 
-        const finalResult = document.createElement('div');
+        // body.appendChild(displayScore);
+        // displayScore.appendChild(displayPlayer);
+        // displayScore.appendChild(displayComputer);
+
+        // const finalResult = document.createElement('div');
         if(finished) {
-            finalResult.textContent = result;
-            displayScore.appendChild(finalResult);
+            final.textContent = result;
 
-            let buttons = document.querySelectorAll('button');
+            let buttons = document.querySelectorAll('input');
             buttons.forEach((button) => {
                 button.disabled = true;
             })
